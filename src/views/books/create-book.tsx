beba487c9e5adaf8b90b75be61ad04c9f169c15f
Jacob Klymenko <klymenkojacob@gmail.com>
2025-06-08 19:42:25 -0700

    fix: remove unused import

--- src/views/books/create-book.tsx
+++ src/views/books/create-book.tsx
@@ -10,7 +10,7 @@ import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Alert, Button, TextField } from '@mui/material';
 import axios from 'utils/axios';
-import { useRouter } from 'next/navigation';
+// import { useRouter } from 'next/navigation';
 
 const defaultTheme = createTheme();
 
